0520b1c96c302d7caa41e57394c89d34a8d4b0a8
atr0x23 <atr0x23@gmail.com>
2021-06-22 12:37:12 +0300

    change the size of map on mobile

--- stores.php
+++ stores.php
@@ -212,7 +212,7 @@
         <!-- on mobile -->
         <div class="w3-bg-grammes-l w3-hide-large w3-hide-medium">
             <div class="w3-content w3-center w3-margin-big-bottom">
-              <iframe src="https://www.google.gr/maps/d/u/1/embed?mid=1PspkCY8MGRXcbBLilGhJMpBavu0D0oWm" width="380" height="500"></iframe>
+              <iframe src="https://www.google.gr/maps/d/u/1/embed?mid=1PspkCY8MGRXcbBLilGhJMpBavu0D0oWm" width="370" height="500"></iframe>
             </div>
         </div>
         
